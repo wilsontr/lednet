69bb0feed117caa07982a67bb8fb12ab92a91fe6
Trevor Wilson <wilsontr@gmail.com>
2019-05-04 20:42:13 -0400

    Send pixel state to websocket

--- server/index.js
+++ server/index.js
@@ -1,45 +1,27 @@
 const _ = require('lodash');
 const WebSocket = require('ws');
 const net = require('net');
-const createParser = require('./parser');
+const createParser = require('./js-opc/parser');
 const createStrand = require('./js-opc/strand');
 
+// const color = {
+//   red: 0,
+//   green: 0,
+//   blue: 111,
+// };
 
-const opcServer = net.createServer(connection => {
-  connection.pipe(createParser()).on('data', message => {
-    // console.log('message', message);
-    // Read pixel colors
-    if (message.command === 0) {
-      const strand = createStrand(message.data);
-      for (var i = 0; i < strand.length; i++) {
-        console.log("  Pixel", i, strand.getPixel(i));
-      }
-      console.log('strand', strand.length);
-    }    
-  })
-});
+const padByte = number => String(number).padStart(3, '0');
+const serializeColor = c => `${padByte(c.red)}${padByte(c.green)}${padByte(c.blue)}`;
 
-opcServer.listen(7890);
-
-console.log('opc server started on port 7890');
+let socket = null;
 
 const wss = new WebSocket.Server({ 
   port: 8080,
   clientTracking: true,
 });
+
 console.log('websocket server started on port 8080');
 
-const color = {
-  red: 0,
-  green: 0,
-  blue: 111,
-};
-
-const padByte = number => String(number).padStart(3, '0');
-
-const serializeColor = c => `${padByte(c.red)}${padByte(c.green)}${padByte(c.blue)}`;
-
-let socket = null;
 
 wss.on('connection', (ws, request) => {
   const clientHost = _.get(request, 'rawHeaders.host', 'N/A');
@@ -60,22 +42,54 @@ wss.on('connection', (ws, request) => {
 
 });
 
-setInterval(() => { 
-  color.red += 5;
-  if (color.red >= 255) {
-    color.red = 0;;  
-  }
-  color.blue -= 5;
-  if (color.blue <= 0) {
-    color.blue = 255;
-  }
-  color.green += 2;
-  if (color.green >= 255) {
-    color.green = 0;
-  }
-  if (socket) {
-    const colorString = `c${serializeColor(color)}`;
-    console.log('color', colorString);
-    socket.send(colorString);
-  }
-}, 33);
+
+const opcServer = net.createServer(connection => {
+  connection.pipe(createParser()).on('data', message => {
+    // console.log('message', message);
+    // Read pixel colors
+    if (message.command === 0) {
+      const strand = createStrand(message.data);
+      for (var i = 0; i < strand.length; i++) {
+        // console.log("  Pixel", i, strand.getPixel(i));
+        const [red, green, blue] = strand.getPixel(i);
+        // color.red = r;
+        // color.green = g;
+        // color.blue = b;
+        const color = {red, green, blue};
+
+        if (socket) {
+          const colorString = `c${serializeColor(color)}`;
+          console.log('color', colorString);
+          socket.send(colorString);
+        }        
+      }
+
+      // console.log('strand', strand.length);
+    }    
+  })
+});
+
+opcServer.listen(7890);
+
+console.log('opc server started on port 7890');
+
+
+// setInterval(() => { 
+//   color.red += 5;
+//   if (color.red >= 255) {
+//     color.red = 0;;  
+//   }
+//   color.blue -= 5;
+//   if (color.blue <= 0) {
+//     color.blue = 255;
+//   }
+//   color.green += 2;
+//   if (color.green >= 255) {
+//     color.green = 0;
+//   }
+//   if (socket) {
+//     const colorString = `c${serializeColor(color)}`;
+//     console.log('color', colorString);
+//     socket.send(colorString);
+//   }
+// }, 33);
